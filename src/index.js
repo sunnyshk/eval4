0dff758bbcbddd26ad9817e5bbeba1925e0c6dba
Juned <87689793+mafiacoders@users.noreply.github.com>
2022-03-28 10:49:23 +0530

    final push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-const userController = require('./controllers/user.controllers');
-const loginController = require('./controllers/login.controllers');
-const todoController = require('./controllers/todo.controllers');
+const userController = require("./controllers/user.controllers");
+const loginController = require("./controllers/login.controllers");
+const todoController = require("./controllers/todo.controllers");
 
 app.use("/register", userController);
 app.use("/login", loginController);
-app.use("/register", todoController);
-
+app.use("/todos", todoController);
 
 module.exports = app;
